c9eed2ae0d92611226f40dcaecd6266bbbd3e903
Punit Dharmadhikari <Punit.Dharmadhikari@ioof.com.au>
2023-04-04 16:35:27 +1000

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,12 @@ function $(...args) {
   return args.map((arg) => document.getElementById(arg));
 }
 
+const N_CARS = 1200;
+const N_TRAFFIC = 50;
+const MUTATE = 0.2;
+const TRAFFIC_RANDOM = true;
+const SENSORS_VISIBLE = true;
+
 const [carCanvas, networkCanvas] = $("car-canvas", "network-canvas");
 carCanvas.width = 200;
 networkCanvas.width = 300;
@@ -10,24 +16,18 @@ networkCanvas.width = 300;
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.95);
-const N = 20;
-const cars = generateCars(N);
+
+const cars = generateCars(N_CARS);
 
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
     if (i != 0) {
-      NeuralNetwork.mutate(cars[i].brain, 0.3);
+      NeuralNetwork.mutate(cars[i].brain, MUTATE);
     }
   }
 }
-
-// setInterval(spawnCar, randInt(1000,3000))
-
-// function spawnCar(bestCar) {
-
-// }
 
 function generateTraffic(N) {
   let traffic = [];
@@ -37,7 +37,7 @@ function generateTraffic(N) {
     .forEach(() => {
       let y;
       do {
-        y = randInt(-400, -7500, 200);
+        y = randInt(0, -6500, 200);
       } while (traffic.map((car) => car.y).filter((c) => c === y).length >= 2);
       const car = new Car(
         road.getLaneCenter(randInt(0, road.laneCount, 1)),
@@ -52,21 +52,27 @@ function generateTraffic(N) {
   return traffic;
 }
 
-const traffic = generateTraffic(30);
+const traffic = TRAFFIC_RANDOM
+  ? generateTraffic(N_TRAFFIC)
+  : [
+      new Car(road.getLaneCenter(1), -100, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(0), -300, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(2), -300, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(0), -500, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(1), -500, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(1), -700, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(2), -700, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(1), -900, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(2), -900, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(0), -1100, 30, 50, ControlType.DUMMY, 2),
+      new Car(road.getLaneCenter(1), -1300, 30, 50, ControlType.DUMMY, 2),
+    ];
 
-// const traffic = [
-//   new Car(road.getLaneCenter(1), -100, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(0), -300, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(2), -300, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(0), -500, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(1), -500, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(1), -700, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(2), -700, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(1), -900, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(2), -900, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(0), -1100, 30, 50, ControlType.DUMMY, 2),
-//   new Car(road.getLaneCenter(1), -1300, 30, 50, ControlType.DUMMY, 2),
-// ];
+let reloadCounter = 30;
+
+setInterval(() => {
+  reloadCounter--;
+}, 1000);
 
 animate();
 
@@ -88,7 +94,7 @@ function discard() {
 function generateCars(N) {
   const cars = [];
   for (let i = 1; i <= N; i++) {
-    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, ControlType.AI));
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, ControlType.AI, 4));
   }
   return cars;
 }
@@ -96,6 +102,12 @@ function generateCars(N) {
 function animate(time) {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
+  }
+
+  console.log({reloadCounter, cars: cars.length, traffic: traffic.length})
+
+  if (reloadCounter <= 0) {
+    location.reload();
   }
 
   for (let i = 0; i < cars.length; i++) {
@@ -114,14 +126,28 @@ function animate(time) {
   traffic.forEach((car, i) => {
     if (car.y > bestCar.y + 300) {
       traffic.splice(i, 1);
-      console.log({ traffic: traffic.length });
+      reloadCounter = 30;
+      if (traffic.length <= 0) {
+        if (N_CARS > 1) {
+          save();
+        }
+        location.reload();
+        try{
+        localStorage.setItem("carsSurvived", JSON.stringify([...JSON.parse(localStorage.getItem("carsSurvived")), cars.length]))
+        } catch (e) {
+          console.error("error")
+        }
+      }
     }
   });
 
   cars.forEach((car, i) => {
     if (car.damaged) {
       cars.splice(i, 1);
-      console.log({ cars: cars.length });
+
+      if (cars.length <= 0) {
+        location.reload();
+      }
     }
   });
 
